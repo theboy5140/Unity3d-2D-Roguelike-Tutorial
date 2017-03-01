8289a89d1db1c0a6407c33e4c651083bc47f5d82
TheBoy <theboy5140@gmail.com>
2017-03-01 11:19:50 +0800

    finish map

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -6,110 +6,105 @@ using Random = UnityEngine.Random;
 
 public class BoardManager : MonoBehaviour 
 {
-
-    [Serializable]
-    public class Count
-    {
-        public int minimum;
-        public int maximum;
-
-        public Count(int min, int max)
-        {   
-            minimum = min;
-            maximum = max;
-        }
-    }
-
-    public class Actor
-    {
-        private GameObject gameObject;
-        private Vector2 position;
-        private Vector3 rotation;
-
-        public Actor(GameObject gameObject, Vector2 position, Vector3 rotation)
-        {
-            this.gameObject = gameObject;
-            this.position = position;
-            this.rotation = rotation;
-        }
-
-        public Actor(GameObject gameObject, Vector2 position)
-        {
-            this.gameObject = gameObject;
-            this.position = position;
-            this.rotation = Vector3.zero;
-        }
-
-        public void SetParentTransform(Transform parent)
-        {
-            this.gameObject.transform.parent = parent;
-        }
-    }
-
-    public int columns = 10;
-    public int rows = 10;
-    public Count wallCountRange = new Count(5, 9);
-    public Count foodCountRange = new Count(1, 5);
-    public Count enemyCountRange = new Count (1, 10);
+    public int width = 10;
+    public int height = 10;
+    public int foodTime = 1;
+    public int enemyTime = 1;
+    public int wallTime = 1;
 
     public GameObject exitTile;
-    public GameObject[] enemyTile;
     public GameObject[] floorTile;
-    public GameObject[] outerWallTile;
     public GameObject[] wallTile;
+    public GameObject[] outerWallTile;
     public GameObject[] foodTile;
+    public GameObject[] enemyTile;
 
-    private GameObject board;
+    private int level = 1;
+    private List<Vector3> positions = new List<Vector3>();
 
-    private void SpawnFloor()
+    public void SetUpBoard(int level)
     {
-        for (int i = 1; i < columns - 1; i++) 
+        InitGround ();
+        if (1 < level) 
         {
-            for (int n = 1; n < rows - 1; n++)
+            this.level = level;        
+        }
+        InitLevelItem ();
+    }
+
+    void InitLevelItem()
+    {
+        int enemyCount = level * enemyTime;
+        int foodCount = level * foodTime;
+        int wallCount = level * wallTime;
+
+        for (int i = 0; i < enemyCount; i++)
+        {
+            SpawnRandonTile (enemyTile);
+        }
+
+        for (int n = 0; n < foodCount; n++) 
+        {
+            SpawnRandonTile (foodTile);
+        }
+
+        for (int m = 0; m < wallCount; m++)
+        {
+            SpawnRandonTile (wallTile);
+        }
+    }
+
+    void SpawnRandonTile(GameObject[] tiles)
+    {
+        GameObject tile = tiles[Random.Range(0, tiles.Length)];
+        int positionIndex = Random.Range (0, positions.Count);
+        Vector3 position = positions [positionIndex];
+        positions.RemoveAt (positionIndex);
+        Instantiate (tile, position, Quaternion.identity);
+    }
+
+    void InitGround()
+    {
+        positions.Clear ();
+        
+        for (int i = 0; i < height; i++)
+        {
+            for (int n = 0; n < width ; n++)
             {
-                GameObject floorObject = floorTile[Random.Range(0, floorTile.Length)];
-                GameObject floor = Instantiate (floorObject, new Vector3(i, n, 0.0f), Quaternion.identity);
-                floor.transform.SetParent (board.transform);
+                Vector3 position = new Vector3 (i, n, 0.0f);
+                GameObject tile;
+
+                if (0 == i || 0 == n || height - 1 == i || width - 1 == n) 
+                {
+                    tile = outerWallTile[Random.Range(0, outerWallTile.Length)];
+
+                } else
+                {
+                    tile = floorTile[Random.Range(0, floorTile.Length)];
+                    positions.Add (position);
+                }
+                Instantiate (tile, position, Quaternion.identity);
             }
         }
     }
 
-    private void SpawnOuterWalls()
+    void Start()
     {
+        SetUpBoard (1);
     }
 
-    private void SpawnWalls()
-    {}
-
-    private void SpawnFood()
+    void Update()
     {
+        
     }
 
-    private void SpawnEnemy()
+    void FixedUpdate()
     {
+        
     }
 
-    private void InitBoard()
+    void LateUpdate()
     {
-        SpawnFloor ();
-        SpawnOuterWalls ();
-        SpawnWalls ();
-        SpawnFood ();
-        SpawnEnemy ();
+        
     }
-
-    // Use this for initialization
-	void Start () 
-    {
-        board = new GameObject ("Board");
-        Instantiate (exitTile, new Vector3 (0, 0, 0f), Quaternion.identity);
-
-        InitBoard ();
-    }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
 }
